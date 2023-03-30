5f1bfa3552af97690081430743ab61746f1a9263
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-03-30 18:31:26 -0300

    Criação de container

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/telas/Cesta.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Cesta />
       <Text>Alura!</Text>
       <StatusBar style="auto" />
     </View>
